e234e806533482c508c264bfa21931254c1b31cf
Zexty Grey <pablo.anneroquebrun@gmail.com>
2026-09-07 16:36:40 +0200

    add a csv compatybility for alcool

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ boutons.forEach(function(bouton){
         document.getElementById(cible).classList.add('active');
     });
 });
+
+fetch('./src/alcool.csv')
+	.then(response => response.text())
+		.then(csvTexte => {
+			const tableau = document.getElementById('tableau-csv-alcool');
+			let html = '';
+			const lignes = csvTexte.trim().split('\n');
+				lignes.forEach((ligne, index) => {
+				const colonnes = ligne.split(','); 
+				
+				html += '<tr>';
+				colonnes.forEach((cellule, numColonne) => {
+					const texteNettoye = cellule.trim();
+					if (index === 0) {
+						html += `<th>${cellule.trim()}</th>`;
+					} else {
+						if (numColonne === 1) {
+							html += `<td><img src="${texteNettoye}" alt="Image produit" style="max-width: 100px; height: auto;"></td>`;
+						} else {
+							html += `<td>${cellule.trim()}</td>`;
+						}
+					}
+				});
+				html += '</tr>';
+			});
+			tableau.innerHTML = html;
+		})
+		.catch(erreur => console.error("Impossible de charger le CSV :", erreur));
